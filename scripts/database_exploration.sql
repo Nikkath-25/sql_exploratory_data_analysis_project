51f31859331a4c8a44ef5e6b368247c7c36111b0
Nikkath <nikkathakram25@gmail.com>
2025-12-23 15:50:32 +0400

    edited scripts and README

--- scripts/database_exploration.sql
+++ scripts/database_exploration.sql
@@ -1,10 +1,30 @@
 /*
-Explore the structure of database 
+Explore the structure of database using 'INFORMATION_SCHEMA'
 */
 
 -- Explore all Objects in Database.
 select * from information_schema.tables;
 
--- Explore all Columns in Database.
+-- Explore all Tables in One Database
+SELECT *
+FROM information_schema.tables
+WHERE table_schema = 'gold_layer';
+
+SELECT table_name
+FROM information_schema.tables
+WHERE table_schema = 'gold_layer';
+
+-- Explore all Columns in Database
+SELECT *
+FROM information_schema.columns
+WHERE table_schema = 'gold_layer';
+
+-- Explore all Columns in Table
 select * from information_schema.columns
-where table_name = 'dim_customers'; 
+where table_schema = 'gold_layer'
+and table_name = 'dim_customers'; 
+
+-- For Quick Checks
+show databases;
+show tables from gold_layer;
+describe gold_layer.dim_customers;
